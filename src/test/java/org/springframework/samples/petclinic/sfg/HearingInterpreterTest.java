9143ba16a36c9d980277b47f24c5941e6827a9e3
helen <hhma@alumni.cpp.edu>
2022-07-10 10:51:41 -0400

    add @ExtendWith(SpringExtension.class)

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -1,17 +1,17 @@
 package org.springframework.samples.petclinic.sfg;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ActiveProfiles("base-test")
-@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
+@ExtendWith(SpringExtension.class)
 public class HearingInterpreterTest {
 
     @Autowired
@@ -19,9 +19,8 @@ public class HearingInterpreterTest {
 
     @Test
     public void whatIheard() {
+
         String word = hearingInterpreter.whatIheard();
-
-
         assertEquals("Laurel", word);
     }
 }
--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
@@ -1,17 +1,18 @@
 package org.springframework.samples.petclinic.sfg;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @ActiveProfiles("base-test")
-@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {BaseConfig.class, YannyConfig.class})
+@ExtendWith(SpringExtension.class)
 public class HearingInterpreterYannyTest {
 
     @Autowired
